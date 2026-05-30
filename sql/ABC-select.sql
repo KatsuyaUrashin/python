a93c4b12e338e779ccd56db1f8931a7a9b799153
Katsuya Urashin <katsuya.urashin@nifty.com>
2026-05-30 21:21:42 +0900

    ABC

--- sql/ABC-select.sql
+++ sql/ABC-select.sql
@@ -1,7 +1,8 @@
 select
-    SHAIN_CD,
-    SHIMEI,
-    TO_CHAR(UMARE, 'YYYY/MM/DD') AS UMARE
+    SHAIN_CD AS 社員コード,
+    SHIMEI AS 氏名,
+    TO_CHAR(UMARE, 'YYYY/MM/DD') AS 生年月日,
+    DECODE(SEX, '1', '男性', '2', '女性', '不明') AS 性別
 from ABC
 where 1=1
 /*$SHAIN_CD*/       AND SHAIN_CD =    :SHAIN_CD
@@ -10,3 +11,5 @@ where 1=1
 /*$UMARE*/          AND UMARE    =    TO_DATE(:UMARE, 'YYYY/MM/DD')
 /*$UMARE_FROM*/     AND UMARE    >=   TO_DATE(:UMARE_FROM, 'YYYY/MM/DD')
 /*$UMARE_TO*/       AND UMARE    <=   TO_DATE(:UMARE_TO, 'YYYY/MM/DD')
+/*$SEX*/            AND SEX       =   :SEX
+order by SHAIN_CD
